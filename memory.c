553af27cf1653e31c7ad5df7e65fa571ac740fd1
Binil-V-B <binil27anoor@gmail.com>
2023-02-10 21:39:44 +0530

    Update memory.c

--- memory.c
+++ memory.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
 
 struct process{
-    int size;
-    int memory_used;
-    int balance;
+    int size;  // used to store the size of each process
+    int allocated; // to check whether a process was allocated or not and also stores the block size to which the process was allocatted
 };
 
 
 void main(){
-    int nb,;
+    int nb,np;
     printf("enter the no of memory blocks: ");
     scanf("%d",&nb);
     int block[nb];
@@ -18,6 +17,27 @@ void main(){
         scanf("%d",&block[i]);
     }
     printf("enter the no of processes: ");
-    scanf("%d",);
+    scanf("%d",&np);
+    struct process pro[np];
+    for (int i = 0; i < np; i++)
+    {
+        scanf("%d",pro[i].size);
+        pro[i].allocated=-1;
+    }
+
+    for (int  i = 0; i < np; i++)
+    {
+        for (int  j = 0; j < nb; j++)
+        {
+            if(pro[i].size<=block[j]){
+                pro[i].allocated=block[j];
+                block[j]=block[j]-pro[i].size;
+                break;
+            }
+        }   
+    }
+    
+    
+    
     
 }
