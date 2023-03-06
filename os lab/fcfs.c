19deb9a5777369828248b45a1a0ab219c3746f27
Binil-V-B <binil27anoor@gmail.com>
2023-03-06 19:59:00 +0530

    typing the first lines

---
+++ os lab/fcfs.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+struct pro{
+    int pid,at,bt,ct,tat,wt;
+};
+
+void main(){
+    int a;
+    printf("Enter the no of processes: ");
+    scanf("%d",&a);
+    struct pro ar[a];
+    read(ar,a);
+}
+
+void read(struct pro a[],int n){
+    for(int i=0;i<){}
+
+}
