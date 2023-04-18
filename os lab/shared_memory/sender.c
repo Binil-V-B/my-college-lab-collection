d18231746c0ab03eca1e1ac091cb13095cfaf945
binil <binil@robinhood.com>
2023-04-18 21:19:37 +0530

    completed sender.c

--- os lab/shared_memory/sender.c
+++ os lab/shared_memory/sender.c
@@ -1,2 +1,18 @@
 #include<stdio.h>
-void main(){}
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include<unistd.h>
+
+void main(){
+	int shmid;
+	char *s;
+	key_t k;
+	k=ftop('a',5);
+	shmid=shmget(k,1024,IPC_CREAT|0666);
+	s=shmat(shmid,NULL,0);
+	printf("Enter a message: ");
+	gets(s);
+	while(*s!='*'){sleep(2);}
+	shmdt(s);
+	shmctl(shmid,IPC_RMID,0);
+}
