5433374ab4ab275c8d0837c7fde289e96e1ac3a3
roman <hellobitches@h.com>
2023-05-19 22:01:59 +0530

    added receiver.c

---
+++ os lab/shared_memory/receiver.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include<unistd.h>
+
+void main(){
+	int shmid;
+	char *s;
+	key_t k;
+	k=ftop('a',5);
+	shmid=shmget(k,1024,IPC_CREAT|0666);
+	s=shmat(shmid,NULL,0);
+	printf("%d",s);
+    s='*';
+	shmdt(s);
+	shmctl(shmid,IPC_RMID,0);
+}
