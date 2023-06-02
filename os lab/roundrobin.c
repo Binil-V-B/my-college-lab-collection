e8a953c5fe5d14f9911b5e32d36d673e84f739f9
lunatic <itsmebinil@rob.com>
2023-06-02 20:18:12 +0530

    modified roundrobin

---
+++ os lab/roundrobin.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+struct process{
+    int at,bt,ct,tat,wt;
+};
+
+void main(){
+
+
+}
+
+void read(){
+    int n;
+    printf("enter the no of process: ");
+    scanf("%d",&n);
+    for (int i = 0; i < n; i++)
+    {
+        
+    }
+    
+}
