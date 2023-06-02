70cde026839ff9778e9f5d1db0a98cbfe0362567
lunatic <itsmebinil@rob.com>
2023-06-02 20:33:22 +0530

    .

--- os lab/roundrobin.c
+++ os lab/roundrobin.c
@@ -3,19 +3,28 @@
 struct process{
     int at,bt,ct,tat,wt;
 };
+typedef struct process pro;
 
-void main(){
 
-
-}
-
-void read(){
-    int n;
-    printf("enter the no of process: ");
-    scanf("%d",&n);
+void read(pro ar[],int n){
     for (int i = 0; i < n; i++)
     {
-        
+	printf("enter the arrival time of process no %d:",i+1);
+	scanf("%d",&ar[i].at);
     }
-    
+}
+
+
+
+
+
+
+
+
+void main(){
+	int n;
+	printf("enter the no of process: ");
+	scanf("%d",&n);
+	struct process ar[n];
+	read(ar[n],n);
 }
