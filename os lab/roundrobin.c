339da4b4e4288d41c6a3acf7c29a5cce85ecf6df
lunatic <itsmebinil@rob.com>
2023-06-02 21:11:52 +0530

    modified roundrobin
    added a function to read the details of all processes

--- os lab/roundrobin.c
+++ os lab/roundrobin.c
@@ -9,12 +9,22 @@ typedef struct process pro;
 void read(pro ar[],int n){
     for (int i = 0; i < n; i++)
     {
-	printf("enter the arrival time of process no %d:",i+1);
+	printf("\nenter the arrival time of process no %d:",i+1);
 	scanf("%d",&ar[i].at);
+    printf("enter the burst time of process no %d:",i+1);
+    scanf("%d",&ar[i].bt);
     }
 }
 
 
+void print(pro ar[],int n){
+    printf("process|\tat|\tbt|\n");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d\t%d\t%d\n",i+1,ar[i].at,ar[i].bt);
+    }
+    
+}
 
 
 
@@ -25,6 +35,7 @@ void main(){
 	int n;
 	printf("enter the no of process: ");
 	scanf("%d",&n);
-	struct process ar[n];
-	read(ar[n],n);
+	pro ar[n];
+	read(ar,n);
+    print(ar,n);
 }
