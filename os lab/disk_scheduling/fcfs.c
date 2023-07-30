7a23529691e7c8281f4808d84a8b0a540d34a078
lunatic <itsmebinil@rob.com>
2023-07-30 08:08:22 +0530

    modified

--- os lab/disk_scheduling/fcfs.c
+++ os lab/disk_scheduling/fcfs.c
@@ -1,4 +1,16 @@
+/*
+read the no of requests - n
+read the request to an array - ar
+read the initial position of the disk
+declare and initialze a variable total head movement to 0
+create a loop to loop through the array
+    total head movement = total head movement + abs(ar[i]-initial)
+    update initial to ar[i]
+    repeat the loop for n
+*/
+
+
 #include<stdio.h>
 void main(){
-    
+
 }
