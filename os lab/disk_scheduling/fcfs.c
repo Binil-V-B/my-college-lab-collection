ddbd4e5dd458864cfb553676fd80f2601c47c0a3
lunatic <itsmebinil@rob.com>
2023-07-30 10:17:27 +0530

    added new code

--- os lab/disk_scheduling/fcfs.c
+++ os lab/disk_scheduling/fcfs.c
@@ -11,6 +11,25 @@ create a loop to loop through the array
 
 
 #include<stdio.h>
+#include<stdlib.h>
 void main(){
-
+    int n,initial,hm=0;
+    printf("enter the no of requests: ");
+    scanf("%d",&n);
+    int ar[n];
+    printf("enter the request one by one:\n");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",&ar[i]);
+    }
+    printf("enter the initial position of the head: ");
+    scanf("%d",&initial);
+    for (int i = 0; i < n; i++)
+    {
+        hm=hm+abs(initial-ar[i]);
+        initial=ar[i];
+    }
+    printf("total head movement = %d\n",hm);
+    
+    
 }
---
+++ os lab/disk_scheduling/scan.c
@@ -0,0 +1,17 @@
+/*
+read the no of disk requests -n
+read all the request to an array -ar
+read the size of the disk -max
+read the initial position of the head -initial
+read the direction -0 or 1
+sort the request in the ascending order using any sorting algorithm
+find the index of request in array which is right next to the initial position of the head
+
+if direction is to right
+    create a loop which starts from the index and iterates to max-1
+    find head movement by adding each
+
+    
+*/
+
+#include<stdio.h>
