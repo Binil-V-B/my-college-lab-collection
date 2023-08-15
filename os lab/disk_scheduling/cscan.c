5c467c02587806a20ff512dbd75e8a4c376f643a
lunatic <itsmebinil@rob.com>
2023-08-15 21:18:32 +0530

    modified

--- os lab/disk_scheduling/cscan.c
+++ os lab/disk_scheduling/cscan.c
@@ -20,6 +20,57 @@
 #include<stdio.h>
 
 void main(){
+    int n,initial,dir,index;
+    printf("enter the no of requests: ");
+    scanf("%d",&n);
+    int ar[n];
+    printf("enter the request sequence:\n ");
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d",&ar[i]);
+    }
 
+    int temp;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n-i-1; j++)
+        {
+            if (ar[i]<ar[i+1])
+            {
+                temp=ar[i+1];
+                ar[i+1]=ar[i];
+                ar[i]=temp;   
+            } 
+        }
+    }
 
+    printf("enter the initial position of the head: ");
+    scanf("%d",&initial);
+    printf("enter the initial head movement:\n 1-high \n 0-low");
+    scanf("%d",&dir);    
+
+    for (int i = 0; i < n; i++)
+    {
+        if (ar[i]>initial)
+        {
+            index=i;
+            break;
+        }
+    }
+
+    
+    
+
+    switch (dir)
+    {
+    case 1 :
+
+        break;
+    
+    case 0 :
+        break;
+
+    default:
+        break;
+    }
 }
