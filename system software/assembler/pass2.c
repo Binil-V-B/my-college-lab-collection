108d4bab9ffbafe2b1468152d7dfa164fd81680a
Binil-V-B <95207111+Binil-V-B@users.noreply.github.com>
2023-12-12 22:29:25 +0530

    Update pass2.c

--- system software/assembler/pass2.c
+++ system software/assembler/pass2.c
@@ -1,22 +1,13 @@
-/*
-!important
-Must Compile and Execute 'pass1.c' before executing this
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 void display();
-// itoa manual implementation as its not ANSI Standard
-//start of itoa block
-// Function to swap two numbers
 void swap(char *x, char *y)
 {
     char t = *x; *x = *y; *y = t;
 }
 
-// Function to reverse `buffer[i…j]`
 char* reverse(char *buffer, int i, int j)
 {
     while (i < j)
@@ -27,16 +18,14 @@ char* reverse(char *buffer, int i, int j)
     return buffer;
 }
 
-// Iterative function to implement `itoa()` function in C
 char* itoa(int value, char* buffer, int base)
 {
-    // invalid input
+ 
     if (base < 2 || base > 32)
     {
         return buffer;
     }
 
-    // consider the absolute value of the number
     int n = abs(value);
 
     int i = 0;
@@ -56,26 +45,21 @@ char* itoa(int value, char* buffer, int base)
         n = n / base;
     }
 
-    // if the number is 0
     if (i == 0)
     {
         buffer[i++] = '0';
     }
 
-    // If the base is 10 and the value is negative, the resulting string
-    // is preceded with a minus sign (-)
-    // With any other base, value is always considered unsigned
     if (value < 0 && base == 10)
     {
         buffer[i++] = '-';
     }
 
-    buffer[i] = '\0'; // null terminate string
+    buffer[i] = '\0'; 
 
-    // reverse the string and return it
     return reverse(buffer, 0, i - 1);
 }
-//end of itoa block
+
 
 int main()
 {
